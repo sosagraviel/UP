928bd5fb87635be5937370b18751f3cbc69857eb
Eduardo Aranda <edar1986@gmail.com>
2020-07-19 17:12:46 -0300

    creating spring-boot application

---
+++ src/main/java/uy/com/urupago/automation/AutomationApplication.java
@@ -0,0 +1,13 @@
+package uy.com.urupago.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationApplication.class, args);
+	}
+
+}
