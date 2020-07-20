5940670b738df9312e9c864dd63d32d780dfde23
Graviel <sosagraviel@gmail.com>
2020-07-19 23:32:25 -0300

    Creating stepsDefinitions

--- src/test/java/uy/com/urupago/automation/runner/CucumberRunner.java
+++ src/test/java/uy/com/urupago/automation/runner/CucumberRunner.java
@@ -2,19 +2,20 @@ package uy.com.urupago.automation.runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {/*".//Feature/Caja.feature",*/".//Feature/Login.feature"/*,".//Feature/Customer.feature""src/test/resources/Features"*/},
+        features = {/*".//Feature/Caja.feature","src/test/resources/Features/Login.feature",".//Feature/Customer.feature"*/"src/test/resources/Features/google.feature"},
         glue = "uy.com.urupago.automation",
         dryRun = false,
-        monochrome = true,
+        //monochrome = true,
         plugin = {
-                "html:reports/default-report", "pretty"
+                "html:reports/default-report", "pretty",
+                "junit:junit_xml_output/cucumber.xml",
+                "rerun:target/rerun.txt"
         }
-        //strict = true,
         //tags = {"@SmokeTest","@RegressionTest"}
 )
 public class CucumberRunner {
